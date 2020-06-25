eb4d60d8e94bf56d103da1287386941370f8b0f2
ninjaaung <ninja.aung02@gmail.com>
2020-06-25 04:32:43 -0700

    Basic Menu Done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void OnPlay() {
+        Debug.Log("'PLAY' Pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnOptions() {
+        Debug.Log("'OPTIONS' Pressed");
+    }
+
+    public void OnQuit() {
+        Debug.Log("'QUIT' Pressed");
+        //Application.Quit();
+        //UnityEditor.EditorApplication.isPlaying = false;
     }
 }
