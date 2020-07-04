4614940da02009e1b219068e60b3bc5aae0fe6cc
JasonWong25th <40476306+JasonWong25th@users.noreply.github.com>
2020-07-03 18:11:57 -0700

    Cleaned up Input Manager & Player script

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DualityES;
-using UnityEngine.InputSystem;
 
 public class CheckForRestart : DualityES.Event
 {
 
 }
 
-public class MouseClickedData : DualityES.Event
-{
-    public bool clicked = false;
-}
 public class KeyboardPressed : DualityES.Event
 {
     public float horizontal = Input.GetAxis("Horizontal");
@@ -29,7 +24,7 @@ public class InputManager : MonoBehaviour
     //public bool inverted = false;
     private int m_invert = 1;
 
-    public bool pasue = false;
+    public bool pause = false;
 
     public static InputManager Instance
     {
@@ -58,109 +53,28 @@ public class InputManager : MonoBehaviour
 
     private void Update()
     {
-
-        if (Input.GetKey(KeyCode.I))
+        EventSystem.instance.RaiseEvent(new KeyboardPressed
         {
-            m_invert *= -1;
-        }
+            horizontal = Input.GetAxis("Horizontal"),
+            vertical = Input.GetAxis("Vertical")
+        });
 
+
+        #region Pause
         if (Input.GetKey(KeyCode.P) || Input.GetKey(KeyCode.Joystick1Button3))
         {
-            if (!pasue)
+            if (!pause)
             {
-                EventSystem.instance.RaiseEvent(new PauseGame { pause = true });
-                pasue = true;
+                //EventSystem.instance.RaiseEvent(new PauseGame { pause = true });
+                pause = true;
             }
             else
             {
-                EventSystem.instance.RaiseEvent(new PauseGame { pause = false });
-                pasue = false;
+                //EventSystem.instance.RaiseEvent(new PauseGame { pause = false });
+                pause = false;
             }
         }
-        switch (GameManager.Instance.Platform)
-        {
-            case Platform.PC:
-                EventSystem.instance.RaiseEvent(new KeyboardPressed
-                {
-                    horizontal = Input.GetAxis("Horizontal"),
-                    vertical = Input.GetAxis("Vertical") * m_invert
-                });
-                break;
-            case Platform.Logitech:
-                EventSystem.instance.RaiseEvent(new KeyboardPressed
-                {
-                    horizontal = Gamepad.current.rightStick.x.ReadValue(),
-                    vertical = Gamepad.current.rightStick.y.ReadValue() * m_invert
-
-                });
-                break;
-        }
-
-
-        //I edited the Input for Fires since there was an error for me if you don't have one change it back to what works
-        if ((GameManager.Instance.Platform == Platform.PC && (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.R))) ||
-            (GameManager.Instance.Platform == Platform.Xbox && Input.GetKeyDown(KeyCode.JoystickButton1))||
-            ((GameManager.Instance.Platform == Platform.PS4 || GameManager.Instance.Platform == Platform.Logitech) && Input.GetKeyDown(KeyCode.JoystickButton2))
-            )//Rush Button
-        {
-            //_rush = true;
-            EventSystem.instance.RaiseEvent(new Rushing
-            {
-                rush = true
-            });
-        }
-        else
-        {
-            //_rush = false;
-            EventSystem.instance.RaiseEvent(new Rushing { rush = false });
-        }
-
-        //The following isn't good enough because time needs to pass before returning back to false
-        if ((GameManager.Instance.Platform == Platform.PC && Input.GetKeyUp(KeyCode.E)) || 
-            ((GameManager.Instance.Platform == Platform.Logitech || GameManager.Instance.Platform == Platform.PS4) && Input.GetKeyUp(KeyCode.JoystickButton0))||
-            (GameManager.Instance.Platform == Platform.Xbox && Input.GetKeyUp(KeyCode.JoystickButton2))
-            )//Eat Button
-        {
-            EventSystem.instance.RaiseEvent(new Eat { eat = true });
-        }
-        else
-        {
-            EventSystem.instance.RaiseEvent(new Eat { eat = false });
-        }
-
-        if (Input.GetButton("Fire3"))
-        {
-            //_pause = !_pause;
-        }
-
-        if ((GameManager.Instance.Platform == Platform.PC && Input.GetMouseButton(0)) || 
-            ((GameManager.Instance.Platform == Platform.PS4 || GameManager.Instance.Platform == Platform.Logitech) && (Input.GetKey(KeyCode.JoystickButton7)) || Input.GetKey(KeyCode.JoystickButton6))||
-            (GameManager.Instance.Platform == Platform.Xbox && (Input.GetAxis("Xbox_RightTrigger")!=0 || Input.GetAxis("Xbox_LeftTrigger") != 0))
-            )
-        {
-            EventSystem.instance.RaiseEvent(new MouseClickedData { clicked = true });
-        }
-        else
-        {
-            EventSystem.instance.RaiseEvent(new MouseClickedData { clicked = false });
-        }
-
-        if (checkRestartButton)
-        {
-
-            if (
-                (Input.GetKey(KeyCode.Space) && GameManager.Instance.Platform == Platform.PC) || 
-                (Input.GetKey(KeyCode.JoystickButton3) && (GameManager.Instance.Platform == Platform.PS4 || GameManager.Instance.Platform == Platform.Logitech))||
-                (Input.GetKey(KeyCode.JoystickButton3) && GameManager.Instance.Platform == Platform.Xbox)
-                )
-            {
-                EventSystem.instance.RaiseEvent(new Restart { });
-            }
-        }
-        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.JoystickButton2) || Input.GetKey(KeyCode.JoystickButton1) || Input.GetKey(KeyCode.JoystickButton3))
-        {
-            EventSystem.instance.RaiseEvent(new OnDismissCollectable { });
-        }
+        #endregion
 
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -37,127 +37,44 @@ public class PlayerPosition : DualityES.Event
 
 public class Player : MonoBehaviour
 {
-    #region Copped from the Low Poly Animal Pack: Wander Script - All the differnet States
-    //Ideally we have a animation manager so we don't have to do it here?
-    [Header("Animation States"), Space(5)]
-    [SerializeField]
-    private IdleState[] idleStates;
-    [SerializeField]
-    private MovementState[] movementStates;
-    [SerializeField]
-    private AnimalState[] attackingStates;
-    [SerializeField]
-    private AnimalState[] deathStates;
-    //To see Gizmos
-    [SerializeField, Tooltip("If true, gizmos will be drawn in the editor.")]
-    private readonly bool showGizmos = true;
-
-    #endregion
-
-    //Player Components
-    private Animator animator;
-    private Rigidbody rb;
-    public Vector3 centerOfMass;
 
 
-    //Used to Calcuate the player's movement
-    Vector3 input;
-    public Vector3 steeringThrust;
-    Vector3 forwardThrust;
-    float forwardThrustAmount;
-
-    //States
-    bool isThrusting = false;
-    bool isBoosting = false;
-    [SerializeField]
-    private bool isHiding = false;
     [SerializeField]
     private bool dead = false; // --> Also used for player & game state
 
     private bool isInControl = true;
     private bool isPauseOn = false;
 
-    [Header("Player Settings"), Space(5)]
-    //Speed Values
-    public float speed = 4f;
-    public float boostSpeed = 6f;
-    public float maxSpeed = 2f;
+    [Header("Player Settings"), Space(2)]
+    public Rigidbody2D rb;
 
-    public float boostTime;
-    float currBoostTime;
-    public float boostLimit;
-    float boostCount;
-    public float boostCoolDown;
-    public float currCoolTime;
+    public float jump_force = 0.0f;
+    public float side_force = 0.0f;
 
-    public float turnForceHorzintal = 3; // This is used in calculating player movement too
-    public float turnForceVertical;
-    public float rotMax;
-    public float rotMin;
+    private float current_speed;
 
-    [Header("Gameplay Stats"), Space(5)]
+
+    [Header("Gameplay Stats"), Space(2)]
     //Player Life Variables
     [SerializeField]
     public float health = 100;
-    [SerializeField]
-    private float hunger = 100;
 
     public void Awake()
     {
-        //Checks if there is an animation added
-        if (idleStates.Length == 0 && movementStates.Length == 0)
-        {
-            Debug.LogError(string.Format("{0} has no idle or movement states state.", gameObject.name));
-            enabled = false;
-            return;
-        }
-
-        rb = GetComponent<Rigidbody>();
-        centerOfMass = rb.centerOfMass;
-        animator = GetComponent<Animator>();
-
-        animator.applyRootMotion = false;
-
-        EventSystem.instance.AddListener<MouseClickedData>(UponMouseClick);
         EventSystem.instance.AddListener<KeyboardPressed>(KeyboardInput);
         EventSystem.instance.AddListener<PlayerDie>(Die);
-        EventSystem.instance.AddListener<OnHealth>(UpdateHealth);
-        EventSystem.instance.AddListener<CollectableEvent>(GotCollectable);
-        EventSystem.instance.AddListener<OnDismissCollectable>(DismissCollectable);
-
-        EventSystem.instance.AddListener<PauseGame>(PauseControl);
 
     }
     private void OnDisable()
     {
-        EventSystem.instance.RemoveListener<MouseClickedData>(UponMouseClick);
         EventSystem.instance.RemoveListener<KeyboardPressed>(KeyboardInput);
         EventSystem.instance.RemoveListener<PlayerDie>(Die);
-        EventSystem.instance.RemoveListener<OnHealth>(UpdateHealth);
-        EventSystem.instance.RemoveListener<CollectableEvent>(GotCollectable);
-        EventSystem.instance.RemoveListener<OnDismissCollectable>(DismissCollectable);
 
-        EventSystem.instance.RemoveListener<PauseGame>(PauseControl);
-    }
-
-    void MoveAnimation(bool state)
-    {
-        if (!string.IsNullOrEmpty(movementStates[0].animationBool))
-        {
-            animator.SetBool(movementStates[0].animationBool, state);
-
-            if(animator.GetBool(movementStates[0].animationBool))
-            {
-                //Debug.Log("Should be moving");
-            }
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        EventSystem.instance.RaiseEvent(new PlayerPosition { position = this.transform.position });
-        EventSystem.instance.RaiseEvent(new UpdatePlayerUI { healhpoints = health, hungerpoints = hunger });
         DeathCheck();
         
     }
@@ -165,172 +82,26 @@ public class Player : MonoBehaviour
     //Is called multiple time per frame, use for physics
     void FixedUpdate()
     {
-//        Debug.Log(transform.rotation.eulerAngles.x);
-        if (isInControl && isPauseOn == false)
-        {
-            if (!dead)
-            {
-                // Restricts player movement so only on input
-                if (isThrusting)
-                {
-                    //Makes sure to update forwardThrustAmount before applying
-                    Boost();
-                    forwardThrust = Vector3.forward * forwardThrustAmount;
-
-                    rb.AddRelativeForce(forwardThrust, ForceMode.Force);
-                }
-
-                Turn();
-            }
-        }
 
     }
-
-    #region Input Functions
     void KeyboardInput(KeyboardPressed keyboardPressedData)
     {
-        if (isInControl && isPauseOn == false)
-        {
-            if (!dead && !GameManager.Instance.pause)
-            {
-                input.Set(
-                 keyboardPressedData.horizontal,
-                 keyboardPressedData.vertical,
-                 0.0f
-                );
-
-                steeringThrust = input.normalized;
-                steeringThrust.x *= turnForceHorzintal;
-                steeringThrust.y *= turnForceVertical;
-            }
-        }
+        current_speed = 0.0f;
 
     }
-
-    void UponMouseClick(MouseClickedData mouseClickedData)
-    {
-        if (isInControl && isPauseOn == false)
-        {
-            if (mouseClickedData.clicked && !dead && !GameManager.Instance.pause)
-            {
-
-                if (!isBoosting)
-                {
-                    forwardThrustAmount = speed;
-                }
-                else
-                {
-                    forwardThrustAmount = boostSpeed;
-                }
-                //Animation
-                isThrusting = true;
-            }
-            else
-            {
-                forwardThrustAmount = 0;
-                isThrusting = false;
-            }
-        }
-
-    }
-    #endregion
-
-    #region 
     public void Die(PlayerDie playerDie)
     {
-        isInControl = false;
+        //isInControl = false;
         dead = true;
         //have to make sure player movement is no existent
-        rb.velocity = Vector3.zero;
-
-        //have to disable all player animations other than death
-        foreach (AnimalState state in idleStates)
-        {
-            if (!string.IsNullOrEmpty(state.animationBool))
-            {
-                animator.SetBool(state.animationBool, false);
-            }
-        }
-
-        foreach (AnimalState state in movementStates)
-        {
-            if (!string.IsNullOrEmpty(state.animationBool))
-            {
-                animator.SetBool(state.animationBool, false);
-            }
-        }
-
-        foreach (AnimalState state in attackingStates)
-        {
-            if (!string.IsNullOrEmpty(state.animationBool))
-            {
-                animator.SetBool(state.animationBool, false);
-            }
-        }
-
-        if (!string.IsNullOrEmpty(deathStates[0].animationBool))
-        {
-            animator.SetBool(deathStates[0].animationBool, true);
-        }
-
-        //Have to collect death data to trigger specific death sequence
-        switch (playerDie.causeOfDeath)
-        {
-            case CausesOfDeath.Acidification:
-                EventSystem.instance.RaiseEvent(new DeathUI() {
-                    cause = CausesOfDeath.Acidification
-                });
-                break;
-            case CausesOfDeath.Deadzones:
-                EventSystem.instance.RaiseEvent(new DeathUI() {  cause = CausesOfDeath.Deadzones });
-                break;
-            case CausesOfDeath.Eaten:
-                EventSystem.instance.RaiseEvent(new DeathUI() { cause = CausesOfDeath.Eaten });
-                break;
-            case CausesOfDeath.Fishnets:
-                EventSystem.instance.RaiseEvent(new DeathUI() {  cause = CausesOfDeath.Fishnets });
-                break;
-            case CausesOfDeath.Starvation:
-                EventSystem.instance.RaiseEvent(new DeathUI() { cause = CausesOfDeath.Starvation });
-                break;
-            default:
-                break;
-        }
-        AmbienceManager.ActivateEvent("LowerBackground");
-
+        //rb.velocity = Vector3.zero;
+        
     }
-    #endregion
 
-    public void UpdateHealth(OnHealth onHealth)
-    {
-        health += onHealth.deltaHealthAmt;
-    }
 
     void DeathCheck() {
-        if (health <= 0 || Input.GetKey(KeyCode.X))
-        {
-            dead = true;
-        }
 
-        if (!dead)
-        {
-            MoveAnimation(isThrusting);
-        }
-        else
-        {
-            EventSystem.instance.RaiseEvent(new PlayerDie() { causeOfDeath = CausesOfDeath.Acidification});
-        }
     }
-    void GotCollectable(CollectableEvent collectable)
-    {
-        isInControl = false;
-    }
-    void DismissCollectable(OnDismissCollectable onDismiss)
-    {
-        isInControl = true;
-    }
-    void PauseControl(PauseGame pauseGame)
-    {
-        isPauseOn = pauseGame.pause;
-    }
+
+
 }
