432f18a22271c5a5d6726820d60b14a5f9ddc082
Nyein Chan <ninja.aung02@gmail.com>
2020-07-09 04:24:53 -0700

    Re-organized player input removed obsolete files and add push & pull mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,101 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DualityES;
+
+public class Player : MonoBehaviour {
+
+	public PlayerController controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	bool grabbed = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+        if (Input.GetButtonDown("Grab"))
+        {
+            grabbed = true;
+
+        } else if (Input.GetButtonUp("Grab"))
+		{
+			grabbed = false;
+		}
 
 
-public class UpdatePlayerUI : DualityES.Event
-{
+	}
 
-}
-
-public class OnHealth : DualityES.Event
-{
-    public float deltaHealthAmt;
-}
-public class PlayerDie : DualityES.Event
-{
-    
-}
-
-public class Player : MonoBehaviour
-{
-
-
-    [SerializeField]
-    private bool dead = false; // --> Also used for player & game state
-
-    private bool isInControl = true;
-    private bool isPauseOn = false;
-
-    private float current_speed;
-
-    private float distancetoGround = 0.0f;
-
-
-    private int jump_count = 0;
-
-    [Header("Player Settings"), Space(2)]
-    private Rigidbody2D rb;
-    public float jump_force = 0.0f;
-    public float side_force = 0.0f;
-
-
-
-
-    [Header("Gameplay Stats"), Space(2)]
-    //Player Life Variables
-    [SerializeField]
-    public float health = 100;
-
-    public void Awake()
-    {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        EventSystem.instance.AddListener<PlayerDie>(Die);
-
-    }
-    private void OnDisable()
-    {
-        EventSystem.instance.RemoveListener<PlayerDie>(Die);
-
-    }
-
-    public bool IsGrounded()
-    {
-        return Physics2D.Raycast(transform.position,Vector2.down);
-    }
-
-
-    public void Die(PlayerDie playerDie)
-    {
-        //isInControl = false;
-        dead = true;
-        //have to make sure player movement is no existent
-        //rb.velocity = Vector3.zero;
-        
-    }
-
-
-    void DeathCheck() {
-
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        DeathCheck();
-        
-    }
-
-    //Is called multiple time per frame, use for physics
-    void FixedUpdate()
-    {
-        bool isGrounded = IsGrounded();
-        rb.AddForce(Vector2.right * current_speed * Time.deltaTime);
-
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            rb.AddForce(Vector2.up * jump_force * Time.deltaTime, ForceMode2D.Impulse);
-        }     
-    }
-
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+		controller.ObstacleGrab(grabbed);
+	}
 }
