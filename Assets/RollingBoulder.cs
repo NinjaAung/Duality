cac1991258963fc2ed0d80b05e0c955f909420d8
DanteBrundage <42014062+DanteBrundage@users.noreply.github.com>
2020-08-06 01:48:07 -0700

    Stuff for Act 2 unfinished

---
+++ Assets/RollingBoulder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBoulder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Roll bitch");
+            this.gameObject.SetActive(true);
+        }
+    }
+}
