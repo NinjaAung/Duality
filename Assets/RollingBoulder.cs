4240f2c704fc864cac218ac5cc8137484053bebd
DanteBrundage <42014062+DanteBrundage@users.noreply.github.com>
2020-08-06 19:03:32 -0700

    rolling boulder functionality

--- Assets/RollingBoulder.cs
+++ Assets/RollingBoulder.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DualityES;
 
 public class RollingBoulder : MonoBehaviour
 {
+    public GameObject boulder;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        boulder.SetActive(false);
     }
 
     private void Update()
@@ -18,10 +20,10 @@ public class RollingBoulder : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Roll bitch");
-            this.gameObject.SetActive(true);
+            boulder.SetActive(true);
         }
     }
 }
